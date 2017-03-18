5226efdbf6759ad4d714992aef21ab1b3cbc8333
shtimotay <tim@MacBook-Air-van-Tim.local>
2017-03-18 23:24:43 +0000

    Camera was improved
    Holding down arrow keys will now continuously move the camera.

--- Game/main.lua
+++ Game/main.lua
@@ -3,7 +3,7 @@ function love.load()
 	 Tileset = love.graphics.newImage('dirtset3.png')
 	 TileW, TileH = 32,32
   local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
-  
+
 BedTileset = love.graphics.newImage('bed2.png')
 	 TileW2, TileH2 = 160,96
   local tilesetW2, tilesetH2 = BedTileset:getWidth(), BedTileset:getHeight()
@@ -19,7 +19,7 @@ math.randomseed(os.time())
 	        	 end
       end
     end
-    
+
     Quads = {}
 Quads[1] = love.graphics.newQuad(0,   0, TileW, TileH, tilesetW, tilesetH) -- 1 = grass
 Quads[2] = love.graphics.newQuad(32,  0, TileW, TileH, tilesetW, tilesetH) -- 2 = box
@@ -39,68 +39,71 @@ Quads[9] = love.graphics.newQuad(0, 0, TileW2, TileH2, tilesetW2, tilesetH2) -- 
 		act_x = 200,
 		act_y = 200,
 		speed = 10,
-	}
-		
-	
-end
+		}
+		mouse={}
+
+		love.keyboard.setKeyRepeat(0.001, 0)
+
+		end
 
 function love.update(dt)
 
-player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
+
+
+
+
 player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
+player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
 
 camera:setPosition(player.grid_x - (love.graphics.getWidth()/2), player.grid_y - (love.graphics.getHeight()/2) )
-
-	
-	
+  mouse.x, mouse.y = love.mouse.getPosition()
 end
 
-function love.draw()   
+function love.draw()
 
-  camera:set()
+  camera:set() -- DRAW EVERYTHING AFTER THIS
+
+
 
    for i=1,100 do
       for j=1,100 do
 			  love.graphics.draw(Tileset, Quads[board[i][j]], (j-1)*TileW, (i-1)*TileH)
       end
-    end   
+    end
     for i=1,100 do
       for j=1,100 do
 		      if board[i][j] == 9 then love.graphics.draw(BedTileset, Quads[9],(j-1)*TileW, (i-1)*TileH) end
       end
-    end 
-    
-    love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
-    
-      -- draw stuff
+    end
+--love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
+
+		love.graphics.setColor(255, 255, 255)
+		    love.graphics.print("Mouse Coordinates: " .. mouse.x .. ", " .. mouse.y, 0, 0)
+
+
+
   camera:unset()
- 
+
 end
 
 
-function love.focus(bool) -- if you click out of the game, the game will 'unfocus' and may be used to pause the game if you tab out! (Bool)	
+function love.focus(bool) -- if you click out of the game, the game will 'unfocus' and may be used to pause the game if you tab out! (Bool)
 end
 
-function love.keypressed(key, unicode)
-	if key == "up" then
-		player.grid_y = player.grid_y - 32
-	elseif key == "down" then
-		player.grid_y = player.grid_y + 32
-	elseif key == "left" then
-		player.grid_x = player.grid_x - 32
-	elseif key == "right" then
-		player.grid_x = player.grid_x + 32
-	end
-	
-		if key == "up" then
-		camera:move(0, 32)
-	elseif key == "down" then
-		camera:move(0, -32)
-	elseif key == "left" then
-		camera:move(-32, 0)
-	elseif key == "right" then
-		camera:move(32, 0)
-	end
+function love.keypressed(key, unicode, isrepeat)
+
+if key == "up" then
+player.grid_y = player.grid_y - 32
+end
+if key == "down" then
+player.grid_y = player.grid_y + 32
+end
+if key == "left" then
+player.grid_x = player.grid_x - 32
+end
+if key == "right" then
+player.grid_x = player.grid_x + 32
+end
 
 end
 
@@ -108,11 +111,7 @@ function love.keyreleased (key, unicode)
 end
 
 function love.mousepressed (x, y, button) -- origin is top left, button r is right, l is left
-if button == l then
-local tempx = x/32
-local tempy = y/32
-board[tempy][tempx] = 5
-end
+
 
 end
 
