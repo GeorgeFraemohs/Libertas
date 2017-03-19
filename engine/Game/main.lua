14c4b38d96b6178aee367d3213a55055f521d695
GeorgeFraemohs <GeorgeFraemohs@Georges-MacBook-Pro.local>
2017-03-19 00:23:23 +0000

    Implemented 'Border Control"
    #takebackcontrol ;)

--- engine/Game/main.lua
+++ engine/Game/main.lua
@@ -1,21 +1,18 @@
 require("camera")
 function love.load()
-	 Tileset = love.graphics.newImage('assets/dirtset3.png')
+	 Tileset = love.graphics.newImage('dirtset3.png')
 	 TileW, TileH = 32,32
   local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
-width = {}
-width = love.graphics.getWidth()
-height = {}
-height = love.graphics.getHeight()
-ChasmTileset = love.graphics.newImage('assets/chasm.png')
+
+BedTileset = love.graphics.newImage('bed2.png')
 	 TileW2, TileH2 = 160,96
-  local tilesetW2, tilesetH2 = ChasmTileset:getWidth(), ChasmTileset:getHeight()
+  local tilesetW2, tilesetH2 = BedTileset:getWidth(), BedTileset:getHeight()
 
 math.randomseed(os.time())
   board = {}          -- create the matrix
-    for i=1,100 do    -- if change, change border control
+    for i=1,100 do
       board [i] = {}     -- create a new row
-      for j=1,100 do     -- if change, change border control
+      for j=1,100 do
         board[i][j] = math.random(1,8)
         	      if math.random(1,20) == 1 and i % 3 == 0 and j % 5 == 0 then
 	        	      board[i][j] = 9
@@ -37,36 +34,30 @@ Quads[9] = love.graphics.newQuad(0, 0, TileW2, TileH2, tilesetW2, tilesetH2) -- 
 
 
 	player = {
-		grid_x = 1600,
-		grid_y = 1600,
+		grid_x = 256,
+		grid_y = 256,
 		act_x = 200,
 		act_y = 200,
 		speed = 10,
-		}
-		mouse={}
+	}
 
-		love.keyboard.setKeyRepeat(0.001, 0)
 
-		end
+end
 
 function love.update(dt)
 
-
-
-
-
-player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
 player.act_y = player.act_y - ((player.act_y - player.grid_y) * player.speed * dt)
+player.act_x = player.act_x - ((player.act_x - player.grid_x) * player.speed * dt)
 
 camera:setPosition(player.grid_x - (love.graphics.getWidth()/2), player.grid_y - (love.graphics.getHeight()/2) )
-  mouse.x, mouse.y = love.mouse.getPosition()
+
+
+
 end
 
 function love.draw()
 
-  camera:set() -- DRAW EVERYTHING AFTER THIS
-
-
+  camera:set()
 
    for i=1,100 do
       for j=1,100 do
@@ -75,16 +66,13 @@ function love.draw()
     end
     for i=1,100 do
       for j=1,100 do
-		      if board[i][j] == 9 then love.graphics.draw(ChasmTileset, Quads[9],(j-1)*TileW, (i-1)*TileH) end
+		      if board[i][j] == 9 then love.graphics.draw(BedTileset, Quads[9],(j-1)*TileW, (i-1)*TileH) end
       end
     end
---love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
 
-		love.graphics.setColor(255, 255, 255)
-		    love.graphics.print("Mouse Coordinates: " .. mouse.x .. ", " .. mouse.y, 0, 0)
+    love.graphics.rectangle("fill", player.act_x, player.act_y, 32, 32)
 
-
-
+      -- draw stuff
   camera:unset()
 
 end
@@ -93,24 +81,36 @@ end
 function love.focus(bool) -- if you click out of the game, the game will 'unfocus' and may be used to pause the game if you tab out! (Bool)
 end
 
-function love.keypressed(key, unicode, isrepeat)
+function love.keypressed(key, unicode)
+	if key == "up" then
+		player.grid_y = player.grid_y - 32
+		 if player.grid_y < 0 then player.grid_y = 0; end
+  end
+	if key == "down" then
+		player.grid_y = player.grid_y + 32
+		 if player.grid_y > 3200 then player.grid_y = 3200; end
+  end
+	if key == "left" then
+		player.grid_x = player.grid_x - 32
+		 if player.grid_x < 0 then player.grid_x = 0; end
+  end
+	if key == "right" then
+		player.grid_x = player.grid_x + 32
+		 if player.grid_x > 3200 then player.grid_x = 3200; end
+	end
 
-if key == "up" then
-player.grid_y = player.grid_y - 32
-if player.grid_y < height/2 then player.grid_y = height/2; end
 end
-if key == "down" then
-player.grid_y = player.grid_y + 32
-if player.grid_y > 3200 - (height/2) then player.grid_y = 3200 - (height/2); end
-end
-if key == "left" then
-player.grid_x = player.grid_x - 32
-if player.grid_x < width/2 then player.grid_x = width/2; end
-end
-if key == "right" then
-player.grid_x = player.grid_x + 32
-if player.grid_x > 3200 - (width/2) then player.grid_x = 3200 - (width/2); end
-end
+
+
+		if key == "up" then
+		camera:move(0, 32)
+	elseif key == "down" then
+		camera:move(0, -32)
+	elseif key == "left" then
+		camera:move(-32, 0)
+	elseif key == "right" then
+		camera:move(32, 0)
+	end
 
 end
 
@@ -118,7 +118,11 @@ function love.keyreleased (key, unicode)
 end
 
 function love.mousepressed (x, y, button) -- origin is top left, button r is right, l is left
-
+if button == l then
+local tempx = x/32
+local tempy = y/32
+board[tempy][tempx] = 5
+end
 
 end
 
@@ -129,4 +133,4 @@ end
 
 
 function love.quit()
-end
+end 
